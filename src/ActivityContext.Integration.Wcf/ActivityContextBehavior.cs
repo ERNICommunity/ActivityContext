90320b7c34caa488e9dfe4b6074095364e67dfb1
Michal Dorner <dorner.michal@gmail.com>
2017-08-15 08:42:02 +0200

    Add skeleton of WCF integration project

---
+++ src/ActivityContext.Integration.Wcf/ActivityContextBehavior.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace ActivityContext.Integration.Wcf
+{
+    public sealed class ActivityContextBehavior : IServiceBehavior, IEndpointBehavior
+    {
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void Validate(ServiceEndpoint endpoint)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ActivityContext.Integration.Wcf/ActivityContextMessageInspector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+
+namespace ActivityContext.Integration.Wcf
+{
+    public class ActivityContextMessageInspector : IDispatchMessageInspector, IClientMessageInspector
+    {
+        #region IDispatchMessageInspector
+
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void BeforeSendReply(ref Message reply, object correlationState)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IClientMessageInspector
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
